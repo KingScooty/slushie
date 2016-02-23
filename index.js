27f971a4b23ed5e8e5d9fd2b251aa6c1d11de025
Scotty Vernon <scott.vernon@gmail.com>
2016-02-23 12:20:55 +0000

    Setup linting dependency task.

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 //Sass compile, autoprefix, cssnano, scoping thingy
+
+var linting = require('./tasks/linting');
 var sass_development = require('./tasks/sass_development');
 var sass_production = require('./tasks/sass_production');
-var linting = require('./tasks/linting');
 
-gulp.task('sass:development', function() {
+gulp.task('sass-lint', function() {
+  return linting();
+});
+
+gulp.task('sass:development', ['sass-lint'], function() {
   return sass_development();
 });
 
-gulp.task('sass', ['sass-lint'], function() {
-  return compileSass(gulp);
-});
-
-gulp.task('sass:production', function() {
+gulp.task('sass:production', ['sass-lint'], function() {
   return sass_production();
 });
+
+// Watch task
