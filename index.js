2ff02a94819ad975f522ec510c5302b73e009a8f
Scotty Vernon <scott.vernon@gmail.com>
2016-02-24 15:57:28 +0000

    Update readme.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 
 var _ = require('lodash');
+var runSequence = require('run-sequence');
 
 var linting = require('./tasks/linting');
 var sass_development = require('./tasks/sass_development');
@@ -17,26 +18,40 @@ module.exports = function(user_settings) {
    */
   var settings = _.merge({}, default_settings, user_settings);
 
-  gulp.task('sass-lint', function() {
+  /**
+   * Internal tasks--can be called directly, but are used as part of
+   * the external tasks below.
+   */
+
+  gulp.task('sass:lint', function() {
     return linting();
   });
 
-  gulp.task('sass:development', ['sass-lint'], function() {
+  gulp.task('sass:development', ['sass:lint'], function() {
     return sass_development(settings.sass_development);
   });
 
-  gulp.task('sass:production', ['sass-lint'], function() {
+  gulp.task('sass:production', ['sass:lint'], function() {
     return sass_production(settings.sass_production);
   });
+
+  gulp.task('sass:prefix', ['sass:production'], function() {
+    return gs_prefix_classes();
+  });
+
+  /**
+   * Helper tasks to be used with CLI:
+   */
 
   gulp.task('watch', ['sass:development'], function() {
     gulp.watch(settings.watch.destination, ['sass:development']);
   });
 
-  gulp.task('prefix', ['sass:production'], function() {
-    return gs_prefix_classes();
+  gulp.task('build:production', function(callback) {
+    runSequence(
+      'sass:production',
+      'sass:prefix',
+      callback);
   });
 
 }
-
-// Watch task
---
+++ readme.md
@@ -0,0 +1,27 @@
+#Morph GrandStand Gulp
+This little tool provides a nicely packaged way of compiling Sass files.
+
+##Installing (WIP -- this bit might not work properly yet)
+
+`morph install morph-grandstand-gulp`
+
+Create a `gulpfile.js` in your project with the following:
+
+```js
+var morph_grandstand_gulp = require('morph-grandstand-gulp');
+
+var config = {};
+
+morph_grandstand_gulp(config);
+```
+
+This should work out of the box thanks to the carefully considered defaults set for morph projects. However, if you do want to override any of the presets, take a look at the default settings in  [default.js](https://github.com/bbc/morph-grandstand-gulp/blob/master/defaults.js) and override as necessary in the `config` object in your newly created `gulpfile`.
+
+
+## Main gulp tasks available
+Commands  | Meaning
+------------- | -------------
+`gulp`  | alias for `gulp watch`
+`gulp watch` | Sets up a Sass watch task to lint, and recompile the files in dev/fast mode (uncompressed, unoptimised) on change for development.
+`gulp sass:lint` | Lints the Sass files and reports any errors.
+`gulp build:production`  | Builds the Sass ready for production. Sass files are fully optimised, and minified. Calls sass:production and prefix in sequence internally.
